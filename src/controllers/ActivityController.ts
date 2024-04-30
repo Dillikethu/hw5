afbdf0eb035e2ef424f9e8bb8cfa08965a97804b
Dillikethu <163803225+Dillikethu@users.noreply.github.com>
2024-04-29 19:23:35 -0500

    Delete src directory

--- src/controllers/ActivityController.ts
+++
@@ -1,7 +0,0 @@
-import { Activity } from '../models/ActivityModel';
-
-export const getActivity = async (): Promise<Activity> => {
-  const response = await fetch('https://www.boredapi.com/api/activity');
-  const data = await response.json();
-  return { activity: data.activity };
-};
--- src/models/ActivityModel.ts
+++
@@ -1,4 +0,0 @@
-export interface Activity {
-    activity: string;
-  }
-  
